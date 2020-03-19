8c852af21a77cf89d63e8d60410ee10caff7723d
Vincentlortie <vincent.lortie@live.ca>
2020-03-19 17:53:48 -0400

    update scripts

--- scripts/keep_clinvar_in_tsv.py
+++ scripts/keep_clinvar_in_tsv.py
@@ -1,9 +1,11 @@
 import csv
 counter = 0
-with open('E:/dbNSFP4.0a_variant.chr1', 'r', 262144) as f:
-    with open('out.tsv', 'w') as o:
+with open('E:/dbNSFP4.0a_variant.chr4', 'r', 262144) as f:
+    with open('chr4.tsv', 'w') as o:
         reader = csv.DictReader(f, delimiter='\t')
-        csvwriter = csv.writer(o)
+        for row in reader:
+            o.write('\t'.join(list(row.keys())) + '\n')
+            break
         for row in reader:
             if row['clinvar_id'] != '.':
                 d = row.values()
--- scripts/keep_relevant_headers.py
+++ scripts/keep_relevant_headers.py
@@ -1,16 +1,39 @@
-import csv
-KEEP_HEADERS = ['chr', 'pos(1-based)', 'ref', 'alt', 'SIFT_pred', 'FATHMM_pred', 'PROVEAN_pred', 'clinvar_clnsig']
 
-counter = 0
-with open('temp.tsv', 'r', 262144) as f:
-    with open('final.tsv', 'w') as o:
-        reader = csv.DictReader(f, delimiter='\t')
-        csvwriter = csv.writer(o)
-        for row in reader:
-            new_row = []
-            for key, val in row.items():
-                if key and key.strip() in KEEP_HEADERS:
-                    new_row.append(val)
-            o.write('\t'.join(new_row) + '\n')
-            counter += 1
-            print(counter)
+import argparse
+import sys
+
+
+import csv
+
+
+def main(inp, out, cols):
+    if cols:
+        KEEP_HEADERS = cols.split(',')
+    else:
+        KEEP_HEADERS = ['chr', 'pos(1-based)', 'ref', 'alt', 'SIFT_pred', 'FATHMM_pred', 'PROVEAN_pred', 'clinvar_clnsig']
+
+
+    with open(inp, 'r', 262144) as f:
+        with open(out, 'w') as o:
+            reader = csv.DictReader(f, delimiter='\t')
+            o.write('\t'.join(KEEP_HEADERS) + '\n')
+            for row in reader:
+                new_row = []
+                for key, val in row.items():
+                    if key and key.strip() in KEEP_HEADERS:
+                        if ';' in val:
+                            preds_arr = val.split(';')
+                            val = max(set(preds_arr), key=preds_arr.count)
+                        new_row.append(val)
+                o.write('\t'.join(new_row) + '\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="""
+    Takes a dbnsfp file and only keeps the specified columns
+    """, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('inp', help='input dbnsfp file')
+    parser.add_argument('out', help='VCF containing fixed rows')
+    parser.add_argument('--cols', required=False, help='The columns we wish to keep(comma separated, with no spaces)')
+    args = parser.parse_args()
+    sys.exit(main(inp=args.inp, out=args.out, cols=args.cols))
+
